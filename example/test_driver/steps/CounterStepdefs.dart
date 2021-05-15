80bacb29a25a60bd7642b8a646ee2a1b7c656764
Martin Mauch <martin.mauch@gmail.com>
2021-05-15 09:38:21 +0200

    Add null safety

--- example/test_driver/steps/CounterStepdefs.dart
+++ example/test_driver/steps/CounterStepdefs.dart
@@ -12,14 +12,14 @@ class CounterStepdefs {
 
   @Then(r'the counter is {int}')
   void theCounterIs(int counter) async {
-    String text = await _world.driver.getText(find.byValueKey(WidgetKeys.valueKey), timeout: Duration(seconds: 1));
+    String text = await _world.driver!.getText(find.byValueKey(WidgetKeys.valueKey), timeout: Duration(seconds: 1));
     assert(counter.toString() == text, 'The value of the counter was $text but should be $counter');
   }
 
   @When(r'I press the button {int} times')
   void iPressTheButtonTimes(int count) async {
     for(var counter = 0; counter < count; counter ++) {
-      await _world.driver.tap(find.byValueKey(WidgetKeys.addButtonKey), timeout: Duration(seconds: 1));
+      await _world.driver!.tap(find.byValueKey(WidgetKeys.addButtonKey), timeout: Duration(seconds: 1));
     }
   }
 }
